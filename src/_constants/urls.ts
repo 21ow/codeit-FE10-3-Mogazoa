22eb2237b526c86029de463978c5440508dd5611
김지윤 <kimjinewn@gmail.com>
2024-12-18 18:37:45 +0900

    chore: base url 및 환경 변수 추가

---
+++ src/_constants/urls.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const USER_URL = `${BASE_URL}/users`;
+export const REVIEW_URL = `${BASE_URL}/reviews`;
+export const PRODUCT_URL = `${BASE_URL}/products`;
+export const OAUTH_URL = `${BASE_URL}/oauthApps`;
+export const IMAGE_URL = `${BASE_URL}/images`;
+export const FOLLOW_URL = `${BASE_URL}/follow`;
+export const CATEGORY_URL = `${BASE_URL}/categories`;
+export const AUTH_URL = `${BASE_URL}/auth`;
