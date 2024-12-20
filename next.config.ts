32dc3db270e167be17600b8395e562adda1904f3
수진 <hmini@MINIM.local>
2024-12-20 17:09:25 +0900

    chore: next-config 수정

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src')],
+    additionalData: `@use '@/assets/variables' as *;`,
   },
 };
 
