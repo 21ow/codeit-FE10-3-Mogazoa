e53fce37f8c9a47dd07c46b9912a25c4f8a24a0b
minim1nh <hmini206@naver.com>
2024-12-21 17:38:55 +0900

    feat: 공용 버튼 UI 구현 #11 (#25)
    Co-authored-by: 수진 <hmini@MINIM.local>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-// import styles from "./page.module.scss";
+'use client';
 
-export default function Home() {
-  return <div>home</div>;
-}
+import Button from '@/shared/button/Button';
+import styles from './page.module.scss';
+
+const Page = () => {
+  return (
+    <div className={styles.buttonContain}>
+      <Button
+        label="Click me"
+        onClick={() => console.log('안녕하세요')}
+        className={styles.customButton}
+      />
+      <Button
+        label="Click button"
+        onClick={() => console.log('안녕하십니까')}
+        className={styles.customButton2}
+      />
+    </div>
+  );
+};
+
+export default Page;
