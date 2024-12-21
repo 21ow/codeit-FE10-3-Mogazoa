2dbdd0521d0e26f454ae520b772fcd9b190568e2
수진 <hmini@MINIM.local>
2024-12-21 15:35:10 +0900

    chore: pull 오류

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-'use client';
+// import styles from "./page.module.scss";
 
-import Button from '@/shared/button/Button';
-import styles from './page.module.scss';
-
-const Page = () => {
-  return (
-    <div className={styles.buttonContain}>
-      <Button
-        label="Click me"
-        onClick={() => console.log('안녕하세요')}
-        className={styles.customButton}
-      />
-      <Button
-        label="Click button"
-        onClick={() => console.log('안녕하세요')}
-        className={styles.customButton2}
-      />
-    </div>
-  );
-};
-
-export default Page;
+export default function Home() {
+  return <div>home</div>;
+}
