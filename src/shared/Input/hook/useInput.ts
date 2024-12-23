39fba9880e267d54f63edf9a19f73354ef4b4f37
김지윤 <kimjinewn@gmail.com>
2024-12-23 19:41:43 +0900

    feat: 이미지 업로드 로직 추가
    - 최대 업로드 개수 제한 추가
    - 중복 업로드, 선택한 순서 별 업로드 가능
    - 최대 개수 넘을 경우 에러 처리

--- src/shared/Input/hook/useInput.ts
+++ src/shared/Input/hook/useInput.ts
@@ -1,14 +1,14 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const useInput = () => {
+const useInput = (multiple: boolean | undefined) => {
   const [showPassword, setShowPassword] = useState(false);
-  const [filePreviews, setFilePreviews] = useState<string[] | null>(null);
+  const [filePreviews, setFilePreviews] = useState<string[]>([]);
   const [inputKey, setInputKey] = useState<number>(0);
   const [fileErrorMessage, setFileErrorMessage] = useState<string | null>(null);
 
-  // const MAX_FILES = 4;
+  const MAX_FILES = multiple ? 4 : 1;
 
   const handleTogglePassword = () => {
     setShowPassword((prev) => !prev);
@@ -19,11 +19,6 @@ const useInput = () => {
     if (!files) return;
 
     const fileList = Array.from(files);
-
-    // if (filePreviews && filePreviews.length + fileList.length === MAX_FILES) {
-    //   setFileErrorMessage(`최대 ${MAX_FILES}개까지만 업로드할 수 있습니다.`);
-    //   return;
-    // }
 
     const validFiles = fileList.filter((file) => file.size <= 5 * 1024 * 1024);
     const invalidFiles = fileList.filter((file) => file.size > 5 * 1024 * 1024);
@@ -47,7 +42,21 @@ const useInput = () => {
 
     Promise.all(filePromises)
       .then((fileUrls) => {
-        setFilePreviews(fileUrls);
+        setFilePreviews((prev) => {
+          const totalFiles = prev.length + fileUrls.length;
+
+          if (totalFiles > MAX_FILES) {
+            if (MAX_FILES === 1) return fileUrls;
+
+            setFileErrorMessage(
+              `최대 ${MAX_FILES}개까지만 업로드할 수 있습니다.`
+            );
+            return prev;
+          }
+          setFileErrorMessage(null);
+          return [...prev, ...fileUrls];
+        });
+
         setInputKey((prevKey) => prevKey + 1);
       })
       .catch((error) => {
@@ -55,19 +64,16 @@ const useInput = () => {
         setFileErrorMessage(error);
       });
 
-    console.log(filePreviews?.length, fileList.length);
+    console.log('파일 리스트: ', files);
   };
 
   const handleDeleteImg = (index: number) => {
     setFilePreviews((prev) => prev && prev.filter((_, i) => i !== index));
+    setFileErrorMessage(null);
     setInputKey((prevKey) => prevKey + 1);
   };
 
-  // 파일 개수 제한, 파일 중복 선택 가능
-  // 배열에 자리가 있을 때 추가로 이미지를 업로드하면 기존 배열이 전부 삭제되는 문제
-  // 프리뷰랑 파일리스트랑 불일치 문제
-  // 파일 선택한 순서가 아니라, 로컬 순서대로? 업로드 되는 문제
-  // 업로드 된 파일이 있는 상태에서 라벨을 누르면 프리뷰 초기화가 안되는 문제
+  useEffect(() => console.log('파일 프리뷰: ', filePreviews));
 
   return {
     showPassword,
