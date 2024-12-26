098adfa9e6a61f4492e05c62938aebc3c0bb9269
김지윤 <kimjinewn@gmail.com>
2024-12-26 19:58:23 +0900

    chore: 아이콘 경로 수정

--- src/shared/Input/component/PwToggleBtn.tsx
+++ src/shared/Input/component/PwToggleBtn.tsx
@@ -22,14 +22,14 @@ const PwToggleBtn = ({
     >
       {showPassword ? (
         <Image
-          src={'/icon/ic-invisibility.svg'}
+          src={'/icon/ic-visibility.svg'}
           alt={'비밀번호 숨기기 버튼'}
           width={24}
           height={24}
         />
       ) : (
         <Image
-          src={'/icon/ic-visibility.svg'}
+          src={'/icon/ic-invisibility.svg'}
           alt={'비밀번호 보기 버튼'}
           width={24}
           height={24}
