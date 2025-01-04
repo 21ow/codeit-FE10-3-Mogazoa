77c1096ceafc8ca0049fd943825dd450104007a1
김지윤 <kimjinewn@gmail.com>
2025-01-04 20:36:53 +0900

    feat: 루트 페이지 임시 레이아웃 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-const Page = () => {
-  return <div>공룡</div>;
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { getAccessToken } from '@/api/storage/authStorage';
+
+const RootPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = getAccessToken();
+
+    if (!token) {
+      router.push('/login');
+    } else {
+      router.push('/home');
+    }
+  }, [router]);
+
+  return <div>로딩 중...</div>;
 };
 
-export default Page;
+export default RootPage;
