950dada7040f53258155187ee1d03aca4b41b2aa
김지윤 <kimjinewn@gmail.com>
2025-01-04 20:36:54 +0900

    chore: UI 디자인 수정

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from 'react';
+import AddItem from '/public/icon/ic-add-item.svg';
+import Compare from '/public/icon/ic-compare.svg';
 import Navigation from '@/shared/navigation/Navigation';
+import styles from './layout.module.scss';
 import Link from 'next/link';
 
 type LayoutProps = {
@@ -11,8 +14,20 @@ const Layout = ({ children }: LayoutProps) => {
     <div>
       <header>
         <Navigation>
-          <Link href="/auth/signin">로그인</Link>
-          <Link href="/auth/signup">회원가입</Link>
+          <Link href="#" className={styles.favoriteWrapper}>
+            <Compare className={styles.compare} />
+            <span>비교하기</span>
+          </Link>
+          <span>|</span>
+          <Link href="#" className={styles.favoriteWrapper}>
+            <AddItem className={styles.favorite} />
+            <span>등록하기</span>
+          </Link>
+          <span>|</span>
+          <div className={styles.auth}>
+            <Link href="/auth/signin">로그인</Link>/
+            <Link href="/auth/signup">회원가입</Link>
+          </div>
         </Navigation>
       </header>
       <main>{children}</main>
