3783565f345a22c48a40db242a3550fc7999a4cf
김지윤 <kimjinewn@gmail.com>
2025-01-04 20:41:12 +0900

    chore: 리액트 쿼리 데브툴 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,30 @@
-import type { Metadata } from 'next';
-import './globals.scss';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Mogazoa',
-};
+import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './globals.scss';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <Head>
+        <title>Mogazoa</title>
+        <meta name="description" content="Mogazoa application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
