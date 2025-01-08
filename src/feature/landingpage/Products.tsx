0f8ed56ebbd8cc4ed795582c889dd2c271028ebb
minim1nh <hmini206@naver.com>
2025-01-08 20:26:53 +0900

    feat: 리뷰어 리스트 추가 (#60)
    Co-authored-by: 수진 <hmini@MINIM.local>

--- src/feature/landingpage/Products.tsx
+++ src/feature/landingpage/Products.tsx
@@ -1,11 +1,15 @@
 import BestProducts from '@/feature/landingpage/ProductsPage/BestProducts';
 import RatingProducts from '@/feature/landingpage/ProductsPage/RatingProducts';
+import RankingList from '@/feature/landingpage/RankingList/RankingList';
 import styles from './Products.module.scss';
 const Page = () => {
   return (
-    <div className={styles.wrapper}>
-      <BestProducts />
-      <RatingProducts />
+    <div className={styles.allWrapper}>
+      <div className={styles.wrapper}>
+        <BestProducts />
+        <RatingProducts />
+      </div>
+      <RankingList />
     </div>
   );
 };
