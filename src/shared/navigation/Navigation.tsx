72d7d071cabe9ee8e19f881e49bb1796742fc487
김지윤 <kimjinewn@gmail.com>
2025-01-08 22:08:52 +0900

    feat: 햄버거 메뉴 모달 추가

--- src/shared/navigation/Navigation.tsx
+++ src/shared/navigation/Navigation.tsx
@@ -1,29 +1,50 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { createQueries } from '@/api/queries';
+import { CategoryResponse } from '@/api/type/Category';
 import classNames from 'classnames';
 import Link from 'next/link';
-import Logo from '/public/image/img-logo.svg';
-import Input from '../Input/Input';
 import Image from 'next/image';
+import Input from '../Input/Input';
 import Button from '../button/Button';
+import Modal from '../modal/Modal';
+import useModalStore from '../modal/useModalStore';
+import Logo from '/public/image/img-logo.svg';
+import Search from '/public/icon/ic-search.svg';
 import styles from './Navigation.module.scss';
 
 type NavigationProps = {
   children: React.ReactNode;
 };
 
+const categoryQuery = createQueries<CategoryResponse[]>(`/categories`);
+
 const Navigation = ({ children }: NavigationProps) => {
-  const [isSearchVisible, setSearchVisible] = useState(false); //팝오버로 수정 예정
+  const [isSearchVisible, setSearchVisible] = useState(false); //수정 예정
+
+  const { data } = useQuery(categoryQuery.all());
 
   const toggleSearch = () => {
     setSearchVisible((prev) => !prev);
     console.log(isSearchVisible);
   };
 
+  const modalId = useRef('menu');
+  const { modals, openModal, closeModal } = useModalStore();
+
+  const handleOpen = () => {
+    openModal(modalId.current);
+  };
+
+  const handleClose = () => {
+    closeModal(modalId.current);
+  };
+
   return (
     <div className={styles.navigation}>
-      <Button className={styles.menu}>
+      <Button className={styles.menuBtn} onClick={handleOpen}>
         <Image
           src={'/icon/ic-menu.svg'}
           width={24}
@@ -32,6 +53,46 @@ const Navigation = ({ children }: NavigationProps) => {
           className={styles.menu}
         />
       </Button>
+
+      {modals[modalId.current]?.isVisible && (
+        <Modal
+          onClose={handleClose}
+          isVisible={modals[modalId.current]?.isVisible}
+          customModalContainerStyle={styles.customModal}
+          customOverlay={styles.customOverlay}
+          customModalContentStyle={styles.customContent}
+          customVisible={styles.customVisible}
+          customHidden={styles.customHidden}
+          customHeader={
+            <div className={styles.customHeader}>
+              <Link href="/" className={styles.modalLogo}>
+                <Logo />
+              </Link>
+              <div className={styles.authLinkWrapper}>
+                <Link href="/auth/signin" className={styles.signinLink}>
+                  로그인
+                </Link>
+                <Link href="/auth/signup" className={styles.signupLink}>
+                  회원가입
+                </Link>
+                {/* 토큰 있으면 사용자 계정 전환 */}
+              </div>
+            </div>
+          }
+        >
+          <>
+            <div>카테고리</div>
+            <ul className={styles.categories}>
+              {data &&
+                data.map((item) => (
+                  <li key={item.id}>
+                    <Button className={styles.categoryBtn}>{item.name}</Button>
+                  </li>
+                ))}
+            </ul>
+          </>
+        </Modal>
+      )}
 
       <Link href="/" className={styles.logo}>
         <Logo />
@@ -47,14 +108,9 @@ const Navigation = ({ children }: NavigationProps) => {
             [styles.visible]: isSearchVisible,
           })}
         >
-          <Image
-            src={'/icon/ic-search.svg'}
-            width={18}
-            height={18}
-            alt="검색하기"
-            className={styles.searchIcon}
-          />
+          <Search />
           <Input
+            type="text"
             className={styles.search}
             placeholder="상품 이름을 검색해 보세요"
           />
@@ -64,16 +120,11 @@ const Navigation = ({ children }: NavigationProps) => {
       </div>
 
       <Button
-        className={styles.mobileSearchButton}
+        className={styles.mobileSearchBtn}
         onClick={toggleSearch}
         aria-label="검색창 열기"
       >
-        <Image
-          src={'/icon/ic-search.svg'}
-          width={24}
-          height={24}
-          alt="검색하기"
-        />
+        <Search />
       </Button>
     </div>
   );
