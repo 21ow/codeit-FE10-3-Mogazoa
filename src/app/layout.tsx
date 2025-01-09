0095034adfd954dedbfcbf52d483f95ea7e4db18
김지윤 <kimjinewn@gmail.com>
2025-01-09 22:28:23 +0900

    feat: 루트 레이아웃 헤더 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,35 @@
-import type { Metadata } from 'next';
-import './globals.scss';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Mogazoa',
-};
+import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { usePathname } from 'next/navigation';
+import Navigation from '@/shared/navigation/Navigation';
+import './globals.scss';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+  const pathname = usePathname();
+  const isAuthPage = pathname?.startsWith('/auth');
+
   return (
-    <html lang="kr">
-      <body>{children}</body>
+    <html lang="ko">
+      <Head>
+        <title>Mogazoa</title>
+        <meta name="description" content="Mogazoa application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          {!isAuthPage && <Navigation />}
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
