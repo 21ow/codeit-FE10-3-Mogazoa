fabbe3c87ac4fb8ab337e01f87e8f7c03db5425f
changhui-chan <163362771+changhui-chan@users.noreply.github.com>
2025-01-16 16:24:21 +0900

    feat: 드롭다운 2차 개발 (#82)

--- src/shared/dropdown/Dropdown.tsx
+++ src/shared/dropdown/Dropdown.tsx
@@ -1,25 +1,101 @@
-import styles from './Dropdown.module.css';
-import useDropdownStore from './useDropdown';
+import { useEffect, useRef } from 'react';
+import styles from './Dropdown.module.scss';
+import useDropdownStore from './useDropdownStore';
 
 interface DropdownProps {
   options: string[];
   dropdownId: string;
+  parentRef: React.RefObject<HTMLElement>;
+  onClose: () => void;
+  customDropdownStyle?: string;
+  buttonClick?: () => void;
+  isVisible?: boolean;
+  customVisible?: string;
+  customHidden?: string;
 }
 
-const Drowndown = ({ options, dropdownId }: DropdownProps) => {
+const Dropdown = ({
+  options,
+  dropdownId,
+  parentRef,
+  customDropdownStyle = '',
+  onClose = () => {},
+  isVisible = false,
+  customVisible = '',
+  customHidden = '',
+}: DropdownProps) => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const { dropdowns } = useDropdownStore();
 
-  const onClick = (value: string) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape' && dropdownRef.current) {
+        dropdownRef.current.classList.remove(customVisible || styles.visible);
+        dropdownRef.current.classList.add(
+          customHidden ? customHidden : styles.hidden
+        );
+        dropdownRef.current.addEventListener('animationend', () => {
+          onClose();
+        });
+        onClose();
+      }
+    };
+
+    const handleClick = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node) &&
+        !parentRef?.current?.contains(e.target as Node)
+      ) {
+        dropdownRef.current.classList.remove(customVisible || styles.visible);
+        dropdownRef.current.classList.add(
+          customHidden ? customHidden : styles.hidden
+        );
+        dropdownRef.current.addEventListener('animationend', () => {
+          onClose();
+        });
+        onClose();
+      }
+    };
+
+    if (isVisible) {
+      document.body.style.overflow = 'hidden';
+    } else document.body.style.overflow = '';
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('click', handleClick);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('click', handleClick);
+      document.body.style.overflow = '';
+    };
+  }, [isVisible, onClose, customVisible, customHidden, parentRef]);
+
+  const handleItemClick = (value: string) => {
     dropdowns[dropdownId].selectedOption = value;
+    onClose();
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(e.key);
+    //화살표로도 드롭다운 컨트롤 할 수 있게 수정
   };
 
   return (
-    <div className={styles.dropdown}>
+    <div
+      ref={dropdownRef}
+      className={`${customDropdownStyle || styles.container} ${customVisible || styles.visible}`}
+      onKeyUp={handleKeyPress}
+      role="button"
+      tabIndex={0}
+    >
       {options.map((option, index) => (
         <div
           className={styles.item}
           key={index}
-          onClick={() => onClick(option)}
+          onClick={() => handleItemClick(option)}
+          tabIndex={index++}
         >
           {option}
         </div>
@@ -28,4 +104,4 @@ const Drowndown = ({ options, dropdownId }: DropdownProps) => {
   );
 };
 
-export default Drowndown;
+export default Dropdown;
