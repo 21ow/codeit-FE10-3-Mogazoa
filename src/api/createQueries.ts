14bd55ac928b240dc58b4720394f6c777212ac14
김지윤 <kimjinewn@gmail.com>
2025-01-18 22:45:50 +0900

    feat: 경로 수정 및 쿼리 옵션 추가 (#101)

--- src/api/createQueries.ts
+++ src/api/createQueries.ts
@@ -1,25 +1,38 @@
 import axiosInstance from '@/lib/axiosInstance';
-import { QueryKey, queryOptions } from '@tanstack/react-query';
+import { QueryClient, QueryKey, queryOptions } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const createQueries = <R>(
   baseURL: string,
   params?: Record<string, string | number>
 ) => ({
-  all: () =>
-    queryOptions({
-      queryKey: params ? [`${baseURL}`, params] : ([`${baseURL}`] as QueryKey),
+  all: () => {
+    const queryKey = params ? [`${baseURL}`, params] : [`${baseURL}`];
+
+    return queryOptions({
+      queryKey: queryKey,
       queryFn: async (): Promise<R> => {
-        const response = await axiosInstance.get<R>(`${baseURL}`, params);
+        const response = await axiosInstance.get<R>(`${baseURL}`, { params });
         return response.data;
       },
-    }),
+      initialData: () => {
+        const cachedData = queryClient.getQueryData<R>(queryKey);
+        return cachedData;
+      },
+      staleTime: 10000,
+      refetchIntervalInBackground: true,
+    });
+  },
 
-  detail: (id: string) =>
-    queryOptions({
+  // detail 역시 객체로 리턴해야 해
+  detail: (id: string) => {
+    return queryOptions({
       queryKey: [`${baseURL}`, id] as QueryKey,
       queryFn: async () => {
         const response = await axiosInstance.get<R>(`${baseURL}/${id}`);
         return response.data;
       },
-    }),
+    });
+  },
 });
