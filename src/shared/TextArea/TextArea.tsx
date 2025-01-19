eeac37851c5fd3cbd2236a8061888566a06ca120
김지윤 <kimjinewn@gmail.com>
2025-01-19 16:12:37 +0900

    feat: onChange 결합

--- src/shared/TextArea/TextArea.tsx
+++ src/shared/TextArea/TextArea.tsx
@@ -1,31 +1,50 @@
 'use client';
 
+import React, { forwardRef } from 'react';
 import TextCount from '../TextCount';
 import useTextCounter from '@/hook/useTextCounter';
+import ErrorMessage from '../input/component/ErrorMessage';
 import styles from './TextArea.module.scss';
 
-const TextArea = ({
-  className,
-  maxLength,
-  ...props
-}: React.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
-  const { text, handleTextCounter } = useTextCounter(maxLength);
-  return (
-    <div className={styles.textareaWrapper}>
-      <textarea
-        maxLength={maxLength}
-        onChange={handleTextCounter}
-        {...props}
-        className={`${styles.textarea} ${className}`}
-      />
-      <TextCount
-        text={text}
-        maxLength={maxLength}
-        className={styles.textCount}
-      />
-    </div>
-  );
-};
+type TextAreaProps = {
+  formErrorMessage?: string | null;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const TextArea = forwardRef<HTMLTextAreaElement | null, TextAreaProps>(
+  ({ maxLength, className, formErrorMessage, ...props }, ref) => {
+    const { text, handleTextCounter } = useTextCounter(maxLength);
+
+    const { onChange, ...rest } = props;
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      if (onChange) {
+        onChange(e);
+      }
+
+      handleTextCounter(e);
+    };
+
+    return (
+      <div className={styles.textareaWrapper}>
+        <textarea
+          ref={ref}
+          maxLength={maxLength}
+          onChange={handleChange}
+          {...rest}
+          className={`${styles.textarea} ${className}`}
+        />
+
+        <ErrorMessage formError={formErrorMessage} />
+
+        <TextCount
+          text={text}
+          maxLength={maxLength}
+          className={styles.textCount}
+        />
+      </div>
+    );
+  }
+);
 
 TextArea.displayName = 'TextArea';
 export default TextArea;
